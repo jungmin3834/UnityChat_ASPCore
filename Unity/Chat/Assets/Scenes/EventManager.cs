765b20ed7908c2f80acc8edfedc75bfdd689d331
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-03-04 20:26:22 +0900

    MessageSpend Click Event 생성 및 Ui 테스트

---
+++ Unity/Chat/Assets/Scenes/EventManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    private UiManager _uiManager;
+
+    private void Awake()
+    {
+        _uiManager = this.GetComponent<UiManager>();
+    }
+
+    public void SendTextMessage_Click()
+    {
+        _uiManager.Ui_SendMessage();
+    }
+}
