212d219753a2d91594f9e79919a6e0c2dd6fa070
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-03-05 10:43:58 +0900

    872. Leaf-Similar Trees

---
+++ ServerASP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ServerASP
+{
+    class Program
+    {
+        [Route("api/[controller]")]
+        public class ProductsController : Controller
+        {
+            [HttpGet("{id}")]
+            public IActionResult GetProduct(int id)
+            {
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
