8151174b917a1aa6640a9547a7ff879dbd9bc4b9
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-03-08 17:56:59 +0900

    Server Update : Group Add 기능 및 Group Send 기능 테스트

--- ServerASP/ServerASP/Hubs/Chathub.cs
+++ ServerASP/ServerASP/Hubs/Chathub.cs
@@ -5,6 +5,19 @@ namespace ServerASP.Hubs
 {
     public class ChatHub : Hub
     {
+
+        public  Task SendMessage(string user, string message)
+        {
+            Clients.Group("_aid").SendAsync("broadcastMessage", "System", $"Group call {message}");
+            return Clients.All.SendAsync("broadcastMessage", user, message);
+        }
+
+       public async Task JoinGroup(string groupName,string message)
+       {
+          await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+          await Clients.Group(groupName).SendAsync("broadcastMessage","System", $"{Context.ConnectionId} joined {groupName}");
+       }
+
         public async Task Send(string name, string message)
         {
             // Call the broadcastMessage method to update clients.
