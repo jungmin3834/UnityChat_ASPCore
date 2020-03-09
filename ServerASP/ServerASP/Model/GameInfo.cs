fdab110e08886a0b8c0fecb5788dd8d11da630e2
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-03-09 14:23:14 +0900

    Server update : Group + GameInfo class 생성

---
+++ ServerASP/ServerASP/Model/GameInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerASP.Model
+{
+    public class GameInfo
+    {
+        private bool _isEnd { get; set; }
+        private string[] _idArray;
+        private int _size = 0;
+        public GameInfo()
+        {
+            _idArray = new string[4];
+        }
+
+        public void SetSize(int size)
+        {
+            _size = size;
+        }
+
+        public string[] GetConnectionArray()
+        {
+            return _idArray;
+        }
+
+        public bool AddConnectionToArray(int idx ,string id)
+        {
+            _idArray[idx] = id;
+            _size++;
+            if (_size.Equals(4))
+                return true;
+            return false;
+        }
+    }
+}
